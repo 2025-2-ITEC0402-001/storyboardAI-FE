2be8c270eea820535f33ad73349436496a54b84b
toothlessdev <toothless042@gmail.com>
2025-11-17 17:22:29 +0900

    feat(main): reflect-metadata 및 onnxruntime-web 초기화 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
+import "reflect-metadata";
+
 import { createRoot } from "react-dom/client";
 
+import * as ort from "onnxruntime-web";
+
 import "@/app/globals.css";
+import "@/app/lib/webgpu.ts";
 
 import App from "./app/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-);
+ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.23.2/dist/";
+ort.env.logLevel = "warning";
+
+createRoot(document.getElementById("root")!).render(<App />);
