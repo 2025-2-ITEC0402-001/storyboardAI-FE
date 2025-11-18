16d134776b31bdf35eb03ad370a2ad02c41cc495
toothlessdev <toothless042@gmail.com>
2025-11-19 01:27:51 +0900

    refactor(auth): KakaoAuthButton의 OAuth URL 구성 방식 개선

--- src/features/auth/containers/KakaoAuthButton.tsx
+++ src/features/auth/containers/KakaoAuthButton.tsx
@@ -1,12 +1,12 @@
 import { KakaoLogo } from "@/shared/assets/KakaoLogo";
+import { DEVICE_TYPE } from "@/shared/constants/deviceType";
 import { API_BASE_URL } from "@/shared/lib/api";
 import { cn } from "@/shared/lib/utils";
 
-// TODO: 배포 시 redirectUrl 환경변수에 따라 변경
-export const KAKAO_OAUTH_URL =
-    API_BASE_URL +
-    "/api/auth/oauth/kakao/login?state=phone&redirectUrl=" +
-    "http://localhost:5173/auth/callback";
+// TODO: 배포주소 반영필요
+const KAKAO_OAUTH_URL = new URL("/api/auth/oauth/kakao/login", API_BASE_URL);
+KAKAO_OAUTH_URL.searchParams.append("redirectUrl", "http://localhost:5173/auth/callback");
+KAKAO_OAUTH_URL.searchParams.append("deviceType", DEVICE_TYPE.COMPUTER);
 
 export interface KakaoAuthButtonProps extends React.ComponentProps<"a"> {
     width?: React.CSSProperties["width"];
@@ -17,7 +17,7 @@ export const KakaoAuthButton = ({ ...props }: KakaoAuthButtonProps) => {
     return (
         <a
             {...props}
-            href={KAKAO_OAUTH_URL}
+            href={KAKAO_OAUTH_URL.toString()}
             className={cn(
                 "bg-[#FEE500] flex items-center justify-center p-3 gap-2 rounded-sm",
                 props.className,
