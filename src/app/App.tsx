3f828f4388c0ec03c310e24f0d113e1bd1f5b5cf
toothlessdev <toothless042@gmail.com>
2025-11-18 17:09:42 +0900

    feat(global): Portal 컴포넌트 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,12 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "@/app/Routes";
 
+import { GlobalPortal } from "@/shared/components/Portal";
+
 export default function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <GlobalPortal.Provider>
+            <RouterProvider router={router} />
+        </GlobalPortal.Provider>
+    );
 }
