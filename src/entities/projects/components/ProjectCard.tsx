2cd5fdb660a0933468c430c7d10a627a4c3a73bf
toothlessdev <toothless042@gmail.com>
2025-11-21 22:04:46 +0900

    feat(project): 프로젝트 카드 및 컨테이너 컴포넌트 구현

--- src/entities/projects/components/ProjectCard.tsx
+++ src/entities/projects/components/ProjectCard.tsx
@@ -1,32 +1,39 @@
 import { useNavigate } from "react-router-dom";
 
-import { Clock4, FolderOpen } from "lucide-react";
+import { FolderOpen } from "lucide-react";
 
-export interface ProjectCardProps {
-    id: string | number;
-    title: string;
-    time: string;
+import type { ProjectSchema } from "@/entities/projects/model/ProjectSchema";
+
+import { cn } from "@/shared/lib/utils";
+
+export interface ProjectCardProps
+    extends Pick<ProjectSchema, "id" | "title" | "description" | "thumbnail"> {
+    className?: string;
 }
 
-export const ProjectCard = ({ project }: { project: ProjectCardProps }) => {
+export const ProjectCard = ({ id, title, description, thumbnail, className }: ProjectCardProps) => {
     const navigate = useNavigate();
 
     return (
         <li
-            key={project.id}
-            className="bg-[#1C1C21] rounded-lg shadow-md overflow-hidden cursor-pointer border-[#2F2F37]/60 border-1"
-            onClick={() => navigate(`/project/${project.id}`)}
+            key={id}
+            className={cn(
+                "bg-[#1C1C21] rounded-lg shadow-md overflow-hidden cursor-pointer border-[#2F2F37]/60 border",
+                className,
+            )}
+            onClick={() => navigate(`/project/${id}`)}
         >
             <figure className="w-full h-48 bg-linear-to-r from-[#7C3AED]/20 to-[#581C87]/20 flex items-center justify-center">
-                <FolderOpen color="#8B5CF6" className="w-12 h-12" />
+                {thumbnail ? (
+                    <img src={thumbnail} alt={title} className="w-full h-full object-cover" />
+                ) : (
+                    <FolderOpen color="#8B5CF6" className="w-12 h-12" />
+                )}
             </figure>
             <figcaption className=" flex flex-col h-20">
                 <div className="p-4 flex flex-col  h-full gap-1.5">
-                    <span className="text-white">{project.title}</span>
-                    <time className="flex items-center gap-1">
-                        <Clock4 className="w-4 h-4 inline-block mr-1 text-gray-500" />
-                        <span className="text-gray-500">{project.time}</span>
-                    </time>
+                    <span className="text-white">{title}</span>
+                    <span className="text-gray-500">{description}</span>
                 </div>
             </figcaption>
         </li>
--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -1,17 +1,32 @@
+import { Suspense } from "react";
+
+import { ProjectCreateButton } from "@/features/manage-project/components/ProjectCreateButton";
+import { ProjectListContainer } from "@/features/manage-project/containers/ProjectListContainer";
+
 import { ProjectListHeaderWidget } from "@/widgets/project-list/ProjectListHeaderWidget";
-import { ProjectListWidget } from "@/widgets/project-list/ProjectListWidget";
 
 export default function ProjectListPage() {
     return (
         <div className="w-full h-screen flex flex-col">
             <ProjectListHeaderWidget />
+
             <main className="flex-1 px-6 bg-foreground">
                 <section className="mt-10 mb-8 gap-2 flex flex-col">
                     <h1 className="text-background text-3xl font-bold">내 프로젝트</h1>
                     <p className="text-gray-400">스토리보드 프로젝트를 관리하고 편집하세요</p>
                 </section>
 
-                <ProjectListWidget />
+                <article>
+                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        <li className="flex items-center justify-center border-2 border-[#2F2F37]/60 rounded-lg hover:border-gray-500 cursor-pointer h-48 bg-[#1C1C21]">
+                            <ProjectCreateButton />
+                        </li>
+
+                        <Suspense>
+                            <ProjectListContainer />
+                        </Suspense>
+                    </ul>
+                </article>
             </main>
         </div>
     );
