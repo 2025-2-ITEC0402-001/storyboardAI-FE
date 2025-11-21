4483643656c533e91ffd82a21cd93fcf381abe84
toothlessdev <toothless042@gmail.com>
2025-11-21 22:03:15 +0900

    feat(project): 프로젝트 생성 모달 구현

--- src/features/manage-project/components/ProjectCreateButton.tsx
+++ src/features/manage-project/components/ProjectCreateButton.tsx
@@ -1,19 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import { Fragment, useState } from "react";
 
 import { Plus } from "lucide-react";
 
+import { NewProjectModal } from "@/features/manage-project/components/NewProjectModal";
+
 export const ProjectCreateButton = () => {
-    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
     return (
-        <button
-            className="flex flex-col flex-1 items-center justify-center cursor-pointer"
-            onClick={() => navigate("/project/1")}
-        >
-            <div className="flex items-center justify-center w-16 h-16 rounded-full bg-[#8E51FF]/20 mb-3">
-                <Plus color="#8B5CF6" className="w-8 h-8" />
-            </div>
-            <span className="text-background text-4">새 프로젝트 만들기</span>
-        </button>
+        <Fragment>
+            <button
+                className="flex flex-col flex-1 items-center justify-center cursor-pointer"
+                onClick={() => setIsOpen(true)}
+            >
+                <div className="flex items-center justify-center w-16 h-16 rounded-full bg-[#8E51FF]/20 mb-3">
+                    <Plus color="#8B5CF6" className="w-8 h-8" />
+                </div>
+                <span className="text-background text-4">새 프로젝트 만들기</span>
+            </button>
+
+            <NewProjectModal isOpen={isOpen} setIsOpen={setIsOpen} />
+        </Fragment>
     );
 };
