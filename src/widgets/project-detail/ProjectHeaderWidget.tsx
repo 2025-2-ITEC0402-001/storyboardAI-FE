f2f79451932f30308a9b287a2207bff5374020bd
Heeyeun <koheeyeun96@gmail.com>
2025-12-01 19:48:08 +0900

    feat(manage-project): 프로젝트 수정 api 헤더 위젯 연동

--- src/widgets/project-detail/ProjectHeaderWidget.tsx
+++ src/widgets/project-detail/ProjectHeaderWidget.tsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import { Save } from "lucide-react";
 
 import { AppLogo } from "@/app/assets/AppLogo";
 
+import { useGetProjectDetailQuery } from "@/features/manage-project/services/getProjectDetail";
+import { useUpdateProjectMutation } from "@/features/manage-project/services/updateProject";
+
 import { Input } from "@/shared/ui/input";
 
 export const ProjectHeaderWidget = () => {
+    const { id: projectId } = useParams<{ id: string }>();
+
+    const { data: project } = useGetProjectDetailQuery(projectId!);
+
+    const { mutate: updateTitle } = useUpdateProjectMutation(projectId!);
+
+    const [title, setTitle] = useState(project.title);
+
+    const handleTitleBlur = () => {
+        if (title !== project.title) {
+            updateTitle({ title });
+        }
+    };
+
+    useEffect(() => {
+        setTitle(project.title);
+    }, [project.title]);
+
     return (
         <header className="h-14 w-full bg-foreground flex items-center px-4 border-b-[0.5px] border-gray-600">
             <div className="flex gap-4 shrink-0 flex-1 items-center">
@@ -15,7 +37,14 @@ export const ProjectHeaderWidget = () => {
                     <h1 className="text-gray-200">STORYBOARD AI</h1>
                 </Link>
 
-                <Input type="text" placeholder="프로젝트 명" className="max-w-[400px]" />
+                <Input
+                    type="text"
+                    placeholder="프로젝트 명"
+                    value={title}
+                    className="max-w-[400px]"
+                    onChange={(e) => setTitle(e.target.value)}
+                    onBlur={handleTitleBlur}
+                />
             </div>
 
             <div>
