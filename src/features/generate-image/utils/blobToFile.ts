d15d5da89fdb62a28b299eaadc29418334fdfb2b
toothlessdev <toothless042@gmail.com>
2025-12-02 22:16:28 +0900

    feat(generate-video): 파일포맷 변환 유틸리티 함수 추가

---
+++ src/features/generate-image/utils/blobToFile.ts
@@ -0,0 +1,3 @@
+export function blobToFile(blob: Blob, fileName: string): File {
+    return new File([blob], fileName, { type: blob.type });
+}
---
+++ src/features/masking/utils/dataUrlToBlob.ts
@@ -0,0 +1,17 @@
+export function dataURLtoBlob(dataurl: string): Blob {
+    const arr = dataurl.split(",");
+    const mimeMatch = arr[0].match(/:(.*?);/);
+
+    if (!mimeMatch) throw new Error("Invalid data URL");
+    const mime = mimeMatch[1];
+
+    const bstr = atob(arr[1]);
+    const n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    for (let i = 0; i < n; i++) {
+        u8arr[i] = bstr.charCodeAt(i);
+    }
+
+    return new Blob([u8arr], { type: mime });
+}
