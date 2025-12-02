d7056ce99cd9fecae2c600605a9c74662bddc886
toothlessdev <toothless042@gmail.com>
2025-12-03 00:15:39 +0900

    feat(project): 프로젝트 상세 페이지 및 위젯 수정

--- src/widgets/project-detail/SidebarWidget.tsx
+++ src/widgets/project-detail/SidebarWidget.tsx
@@ -4,12 +4,16 @@ import type { RootState } from "@/app/store/store";
 
 import { EditForm } from "@/features/generate-image/containers/EditForm";
 import { ImageGenerationForm } from "@/features/generate-image/containers/ImageGenerationForm";
+import { MaskingForm } from "@/features/generate-image/containers/MaskingForm";
 
 export const SidebarWidget = () => {
     const mode = useSelector((state: RootState) => state.layout.mode);
 
     switch (mode) {
-        case "edit":
+        case "masking":
+            return <MaskingForm />;
+        case "edit:video":
+        case "edit:image":
             return <EditForm />;
         case "generate":
         default:
